18fa1b7e169582781e3ad8a4ccc27f187796b333
nithulc <64567836+nithulc@users.noreply.github.com>
2020-04-30 07:28:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import argparse
 from api import *
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-u", "--username", required=True, help="username of account to scan")
+ap.add_argument("-u", "--username", required=true, help="username of account to scan")
 ap.add_argument("-p", "--postscrap", action='store_true', help="scrape all uploaded images info ")
 ap.add_argument("-s", "--savedata", action='store_true', help="save data to file")
 args = vars(ap.parse_args())
